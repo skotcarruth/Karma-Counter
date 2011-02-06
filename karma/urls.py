1eacdd650fbd04a9944b44f3763b1cbd1a2aa960
Skot Carruth <skot@gophilosophie.com>
2011-02-05 18:39:38 -0800

    set up static media, added logo and css

--- karma/urls.py
+++ karma/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,11 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+        # url(r'^favicon\.ico$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'path': 'favicon.ico'}),
+        # url(r'^404/$', direct_to_template, {'template': '404.html'}),
+        # url(r'^500/$', direct_to_template, {'template': '500.html'}),
+    )
